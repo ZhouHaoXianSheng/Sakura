33b263e5251f8a30b7c5cf9addc7ef591ee1da3f
Zhou Hao <1297908523@qq.com>
2020-08-11 16:08:33 +0800

    Sakura To Be Toper

---
+++ consumer-8080/src/test/java/com/sakura/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakura.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
